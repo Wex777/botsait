2669a45cbb89299d54f4824f437bc13bb8fda7d3
Wex777 <wexghost@proton.me>
2023-11-14 19:48:44 +0300

    first commit

---
+++ site/app.js
@@ -0,0 +1,22 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+
+button1.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 1!");
+		item = "1";
+		tg.MainButton.show();
+	}
+});
