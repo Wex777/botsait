26ef27df0a30233f25cc2d661a07b9d114b28157
Wex777 <wexghost@proton.me>
2023-11-14 22:18:05 +0300

    4commit

--- site/app.js
+++ site/app.js
@@ -10,7 +10,7 @@ const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 
 
-button1.addEventListener("click", function(){
+button2.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
 	}
