6ffaff5db40f229a7b1ed29069fd08b4d3fd3917
Wex777 <wexghost@proton.me>
2023-11-20 19:33:49 +0300

    adfff

--- site/app.js
+++ site/app.js
@@ -21,6 +21,11 @@ cardUser.textContent = userID
 
 button1.addEventListener("click", async function(){
 	try {
+		app.use((req, res, next) => {
+			res.header('Access-Control-Allow-Origin', '*');
+			res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+			next();
+		  });
 		// Отправка POST-запроса к вашему серверу
 		const response = await axios.post('https://573a-5-144-77-106.ngrok-free.app/sendMessage');
 		console.log('Response:', response.data);
