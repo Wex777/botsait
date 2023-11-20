d679e9594bdcddf1bbef7c657dd721b39345a176
Wex777 <wexghost@proton.me>
2023-11-20 18:47:12 +0300

    commti

--- site/app.js
+++ site/app.js
@@ -1,5 +1,5 @@
 
-const axios = require('axios');
+
 
 let tg = window.Telegram.WebApp;
 tg.expand();
@@ -9,28 +9,6 @@ tg.MainButton.color = '#2cab37';
 
 let item = {bit:"10000"};
 
-// Замените 'YOUR_BOT_TOKEN' на реальный токен вашего бота
-const botToken = '6067105307:AAFDNNBsD45UN-p9qQTrjqVkhAxqC802TS4';
-
-// Метод, который вы хотите вызвать (например, sendMessage)
-const methodName = 'sendMessage';
-
-// ID чата, куда нужно отправить сообщение
-const chatId = '1710586323';
-
-// Текст сообщения, которое нужно отправить
-const messageText = 'Hello, this is a message from my bot!';
-
-// URL для запроса
-const apiUrl = `https://api.telegram.org/bot${botToken}/${methodName}`;
-
-// Параметры запроса
-const params = {
-  chat_id: chatId,
-  text: messageText,
-};
-
-// Выполнение POST-запроса с помощью axios
 
 
 
@@ -40,15 +18,14 @@ const cardUser = document.getElementById('first_card')
 const userID = `${tg.initDataUnsafe}`
 cardUser.textContent = userID
 
-button1.addEventListener("click", function(){
-	axios.post(apiUrl, params)
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-	
+button1.addEventListener("click", async function(){
+	try {
+		// Отправка POST-запроса к вашему серверу
+		const response = await axios.post('http://localhost:3000/sendMessage');
+		console.log('Response:', response.data);
+	  } catch (error) {
+		console.error('Error:', error);
+	  }
 });
 
 button2.addEventListener("click", function(){
