27b7bec8d7d74a3966a1174a29df3f5ee710ae50
Wex777 <wexghost@proton.me>
2023-11-20 18:42:05 +0300

    commit

--- site/app.js
+++ site/app.js
@@ -1,3 +1,6 @@
+
+const axios = require('axios');
+
 let tg = window.Telegram.WebApp;
 tg.expand();
 
@@ -5,6 +8,30 @@ tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
 let item = {bit:"10000"};
+
+// Замените 'YOUR_BOT_TOKEN' на реальный токен вашего бота
+const botToken = '6067105307:AAFDNNBsD45UN-p9qQTrjqVkhAxqC802TS4';
+
+// Метод, который вы хотите вызвать (например, sendMessage)
+const methodName = 'sendMessage';
+
+// ID чата, куда нужно отправить сообщение
+const chatId = '1710586323';
+
+// Текст сообщения, которое нужно отправить
+const messageText = 'Hello, this is a message from my bot!';
+
+// URL для запроса
+const apiUrl = `https://api.telegram.org/bot${botToken}/${methodName}`;
+
+// Параметры запроса
+const params = {
+  chat_id: chatId,
+  text: messageText,
+};
+
+// Выполнение POST-запроса с помощью axios
+
 
 
 const button1 = document.getElementById('button1');
@@ -14,7 +41,13 @@ const userID = `${tg.initDataUnsafe}`
 cardUser.textContent = userID
 
 button1.addEventListener("click", function(){
-	tg.sendData(JSON.stringify(item));
+	axios.post(apiUrl, params)
+  .then(response => {
+    console.log('Response:', response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 	
 });
 
@@ -32,8 +65,7 @@ button2.addEventListener("click", function(){
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	
-	
-	
-	
 });
+
+
+
