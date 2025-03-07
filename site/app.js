12de107fc9eb8efeff771d00330f36c39da00ead
Wex777 <wexghost@proton.me>
2025-03-07 20:03:59 +0300

    123

--- site/app.js
+++ site/app.js
@@ -1,13 +1,14 @@
-Telegram.WebApp.ready();
+let tg = window.Telegram.WebApp;
+tg.expand();
 
 // Получение данных пользователя
 const user = Telegram.WebApp.initDataUnsafe.user;
-alert("User ID:", user.id);
+
 
 
 // Отправка данных обратно в бота
 document.getElementById('sendDataButton').addEventListener('click', () => {
     const data = { action: 'button_click', message: 'Hello from Web App!' };
     Telegram.WebApp.sendData(JSON.stringify(data));
-    Telegram.WebApp.close(); // Закрыть веб-приложение после отправки
+    
 });
